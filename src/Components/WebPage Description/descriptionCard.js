c44593adb2db26b4ec58d75f3f3afeb7f29dae99
gavinrdodd <gavin.dodd@global.com>
2024-08-28 16:14:23 +0100

    Add description card

---
+++ src/Components/WebPage Description/descriptionCard.js
@@ -0,0 +1,12 @@
+import "./descriptionCard.scss";
+
+function DescriptionCard() {
+  return (
+    <div className="descriptionCard">
+      <span className="descriptionTitle">Why does this webpage exist...</span>
+      <span className="descriptionCopy">This website was created to be a launching point for my web development learning journey. So as I learn new techniques or just want to practice something, you will see it here. I have currently layed it our in card from and will title each card to label the reason for the card. I may change this in the future as I don't want to bx myself in design wise, it just made sense to start this way...</span>
+    </div>
+  );
+}
+
+export default DescriptionCard;
